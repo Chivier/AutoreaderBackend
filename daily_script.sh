88a91f4838f3ef95dcfe55daacbd4467c5c6440a
Chivier Humber <chivier.humber@outlook.com>
2024-08-04 15:06:53 +0100

    feat: daily script

--- daily_script.sh
+++ daily_script.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+ #!/bin/bash
 
-python darxiv/DailyArxiv.py --paper_dir="./data/paper" --ocr_dir="./data/ocr" --metadata_dir="./data/metadata" --index_dir="./data/index"
+ cd /home/chivier/Projects/darxiv
+ DATEVAL=$(date +%Y-%m-%d)
+ echo $DATEVAL
+
+ /home/chivier/opt/miniconda3/envs/darxiv/bin/python darxiv/DailyArxiv.py --paper_dir="./data/paper" --ocr_dir="./data/ocr" --metadata_dir="./data/metadata" --index_dir="./data/index"
+ rsync -avzP --partial -r data/index/index_$DATEVAL victorique:Projects/autoreader-backend/index
+ rsync -avzP --partial -r data/metadata/papers_$DATEVAL.json victorique:Projects/autoreader-backend/metadata
